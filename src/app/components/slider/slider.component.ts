f3ba6680b930582ab04ded4bec172b7f630bb778
PROkrastinator <vladykin.190103@gmail.com>
2022-03-07 15:52:27 +0300

    Initialized slider with NFTs

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Navigation, Pagination, SwiperOptions } from 'swiper';
 
+SwiperCore.use([Navigation, Pagination]);
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent implements OnInit {
+  config: SwiperOptions = {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    navigation: true,
+    pagination: true,
+  };
 
-  constructor() { }
+  slides = new Array(7);
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
